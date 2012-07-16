aa1f5535f75d7b20410a6385848849f52afe156c
Burke Mamlin <burke@openmrs.org>
2012-07-16 17:11:09 -0400

    initial commit

---
+++ g3_user_event_config.php
@@ -0,0 +1,11 @@
+<?php
+
+# Full path to log file containing G3 user events
+$USER_EVENT_LOG_FILE = "g3_user_events.log";
+
+# Relative URL to data
+$USER_EVENT_DATA_URL = "g3_user_event_data.php";
+
+# Size (in bytes) to scan from end of log file
+$USER_EVENT_LOG_TAIL_SIZE = 10000;
+?>
---
+++ g3_user_event_data.php
@@ -0,0 +1,87 @@
+<?php
+include 'g3_user_event_config.php';
+
+class Action
+{
+	public $name;
+	public $time;
+	function Action($name, $time)
+	{
+		$this->name = $name;
+		$this->time = (int)$time;
+	}
+	public function __toString()
+	{
+		return "($this->name, $this->time)";
+	}
+}
+class Interval
+{
+	public $from;
+	public $to;
+	public $milliseconds;
+	function Interval($from, $to)
+	{
+		$this->from = $from;
+		$this->to = $to;
+		$this->milliseconds = $to->time - $from->time;
+	}
+	public function __toString()
+	{
+		return "$this->from, $this->to, $this->milliseconds";
+	}
+}
+
+$handle = fopen($USER_EVENT_LOG_FILE, "r");
+if ($handle)
+{
+	$lastAction = array();
+	$data = array();
+
+	fseek($handle, 0 - $USER_EVENT_LOG_TAIL_SIZE, SEEK_END);
+	fgets($handle); // get to next line
+	while (!feof($handle))
+	{
+		$line = fgets($handle, 1024);
+		if (preg_match("/username=([^\|]+)\|desktopid=([^\|]+)\|userIP=([^\|]+)\|actionname=([^\|]+)\|actiontime=(\d+)/", $line, $matches))
+		{
+			$username = $matches[1];
+			$desktopid = $matches[2];
+			$userip = $matches[3];
+			$actionname = $matches[4];
+			$actiontime = $matches[5];
+			$key = "$username,$desktopid,$userip";
+			$action = new Action($actionname, $actiontime);
+
+			if (isset($lastAction[$key]))
+			{
+				$interval = new Interval($lastAction[$key], $action);
+				if (isset($data[$key]))
+				{
+					$data[$key][] = $interval;
+				}
+				else
+				{
+					$data[$key] = array($interval);
+				}
+			}
+			$lastAction[$key] = $action;
+		}
+	}
+	fclose($handle);
+
+	header('Content-type: text/json');
+	$first = 1;
+	echo "[";
+	foreach (array_keys($data) as $key)
+	{
+		if (!$first)
+		{
+			echo ",";
+		}
+		$first = 0;
+		echo json_encode($data[$key]);
+	}
+	echo "]";
+}
+?>
